1a20565b7502602593bb34a4de89c19ce01bdf4d
Ievgen Zubkov <euginzub@mail.ru>
2017-06-16 07:02:06 +0300

    Add concept

---
+++ Tango.Domain/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tango.Domain
+{
+    public class Entity
+    {
+        /*
+            Concept: 
+                - UI based on Users, Objects, Tasks, Events withing Streams. Each has rich UI widget
+                - Users can trigger events. Events can trigger other events
+                - Users can define types of Events and Objects and Streams
+                - Users can monitor multiple Streams/Objects/Events types
+                - Events and Objects belong to Streams
+                - Events can change Object state
+        */
+    }
+}
